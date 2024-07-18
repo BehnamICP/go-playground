ba99c1e51ea4b8fab1204aebbed30b7209630997
BehnamICP <145721902+BehnamICP@users.noreply.github.com>
2024-07-18 13:16:46 -0700

    Going through the steps :)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -6,28 +6,47 @@ import (
 	"math/rand"
 )
 
-// Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
 	if name == "" {
 		return name, errors.New("empty name")
 	}
-	// Create a message using a random format.
+
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
-// randomFormat returns one of a set of greeting messages. The returned
-// message is selected at random.
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = " " + message + " ====== "
+	}
+
+	return messages, nil
+}
+
 func randomFormat() string {
-	// A slice of message formats.
 	formats := []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
 	}
 
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
 	return formats[rand.Intn(len(formats))]
+}
+
+func TestFunc() int {
+	sum := 0
+
+	for i := 0; i < 4; i++ {
+		sum += i
+	}
+
+	return sum
 }
--- hello/hello.go
+++ hello/hello.go
@@ -8,21 +8,20 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Behnam")
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// A slice of names.
+	names := []string{"XXX", "John", "Darrin", "Alex"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
+
+	result := greetings.TestFunc()
+
+	fmt.Println(result)
 }
